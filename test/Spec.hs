ae833769ef5cd24e16b2d7f32bc7e416d9451ac0
Ignat Insarov <kindaro@gmail.com>
2015-09-30 10:04:08 +0300

    Adjust: Have test cases be trivial.
    Now, we have totally trivial test cases that should always work in the same immediately
    predictable fashion:
    - 1st one succeeds.
    - 2nd one fails because it's false.
    - 3rd one fails because it's undefined.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,10 +5,10 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-    describe "primes" $ do
-        it "says 2 is a prime" $
-            Main.isPrime 2 `shouldBe` True
-        it "says 3 is a prime" $
-            Main.isPrime 3 `shouldBe` True
-        it "says 4 is composite" $
-            Main.isPrime 4 `shouldBe` False
+    describe "trivial" $ do
+        it "says 2 * 2 equals 4" $
+            2 * 2 == 4 `shouldBe` True
+        it "says 2 * 2 equals 5" $
+            2 * 2 == 5 `shouldBe` True
+        it "says 2 * undefined equals 4" $
+            2 * undefined == 4 `shouldBe` True
