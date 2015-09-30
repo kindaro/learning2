38ab18779728e26485453ba05db5078d966ecda2
Ignat Insarov <kindaro@gmail.com>
2015-09-30 07:02:48 +0300

    Milestone: Naive and inefficient prime generator works.

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,20 @@
 module Main where
 
+
+main :: IO ()
 main = return ()
+
+
+isPrime,isPrimeNaive :: Integer -> Bool
+-- relies on primes < x
+isPrime x
+    | x < 5 = isPrimeNaive x
+    | otherwise = undefined
+
+isPrimeNaive x = length (divisorsNaive x) == 2
+
+divisorsNaive :: Integer -> [(Integer, Integer)]
+divisorsNaive n = filter (\x -> snd x == 0) $ map (quotRem n) $ [1..n]
+
+primes :: [Integer]
+primes = filter isPrime [1..]
