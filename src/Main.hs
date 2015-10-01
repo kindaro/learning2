ceaebc1a0358b89411e477fea7818da2d9413e83
Ignat Insarov <kindaro@gmail.com>
2015-10-01 21:16:47 +0300

    Refactor: Reorder pattern matches.
    Order of patterns matters, which we take advantage of now by applying a very general pattern last
    to catch all corner cases at once.

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,21 @@
 module Main where
 
+
 main :: IO ()
 main = return ()
 
 
 compress :: Eq a => [a] -> [a]
-compress [] = []
-compress [x] = [x]
 compress (x1:x2:xs)
-    | x1 == x2 = compress $ x1 : xs
-    | otherwise = x1 : (compress $ x2 : xs)
+    | x1 == x2   =       recur
+    | otherwise  =  x1 : recur
+    where recur = compress $ x2 : xs
+compress xs = xs
+
 
 differential :: Num a => [a] -> [a]
-differential [] = []
-differential [x] = []
 differential (x1:x2:xs) = (x2 - x1) : (differential $ x2 : xs)
-
+differential _ = []
 
 functionBreaks = compress . (map signum) . differential
+
